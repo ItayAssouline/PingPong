a3f35a7ed77b6877b87d8ccefffd363d199e441a
ItayAssouline <Fushionartz@gmail.com>
2022-01-19 17:21:10 +0200

    Exercise A

--- PingPong/PingPong/Program.cs
+++ PingPong/PingPong/Program.cs
@@ -1,15 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PingPong
+namespace PingPong.Server
 {
-	class Program
-	{
-		static void Main(string[] args)
+    class Program
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        public static void ExecuteServer()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using
+            // Socket Class Constructor
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a
+                // network address to the Server Socket
+                // All client that will connect to this
+                // Server Socket must know this network
+                // Address
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create
+                // the Client list that will want
+                // to connect to Server
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting For Connections ... ");
+                while (true)
+				{
+                    
+                    Socket clientSocket = listener.Accept();
+					Console.WriteLine("New Connection! Handling.");
+                    Task.Run(() =>
+                    {
+                        HandleClient(clientSocket);
+                    });
+                }
+                    
+                
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void HandleClient(Socket clientSocket)
 		{
-		}
-	}
+            byte[] bytes = new Byte[1024];
+            string data = string.Empty;
+
+			while (data.IndexOf("<CloseSocket>") == -1)
+			{
+                data = string.Empty;
+
+                while (true)
+                {
+                    int numByte = clientSocket.Receive(bytes);
+
+                    data += Encoding.ASCII.GetString(bytes,
+                                               0, numByte);
+                    if (data.IndexOf("<EOF>") > -1)
+                        break;
+                }
+                string dataParsed = data;
+                if(data.IndexOf("<CloseSocket>") != -1)
+				{
+                   dataParsed = dataParsed.Remove(dataParsed.Length - "<CloseSocket>".Length, "<CloseSocket>".Length);
+                }
+                dataParsed = dataParsed.Remove(dataParsed.Length-6, 6);
+                Console.WriteLine("Text received -> {0} ", dataParsed);
+                byte[] message = Encoding.ASCII.GetBytes(dataParsed);
+
+                // Send a message to Client
+                // using Send() method
+                clientSocket.Send(message);
+
+            }
+			// Close client Socket using the
+			// Close() method. After closing,
+			// we can use the closed Socket
+			// for a new Client Connection
+			Console.WriteLine("Connection Closed");
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
 }
---
+++ PingPongClient/PingPongClient/SocketClient.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PingPongClient
+{
+
+    class Program
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        // ExecuteClient() Method
+        static void ExecuteClient()
+        {
+
+            try
+            {
+                // Establish the remote endpoint
+                // for the socket. This example
+                // uses port 11111 on the local
+                // computer.
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                // Creation TCP/IP Socket using
+                // Socket Class Constructor
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                           SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+
+                    // Connect Socket to the remote
+                    // endpoint using method Connect()
+                    sender.Connect(localEndPoint);
+
+                    // We print EndPoint information
+                    // that we are connected
+                    Console.WriteLine("Socket connected to -> {0} ",
+                                  sender.RemoteEndPoint.ToString());
+
+                    // Creation of message that
+                    // we will send to Server
+                    string userInput = string.Empty;
+
+                    
+                    while (userInput != "-1")
+					{
+                        Console.WriteLine("Enter your message to the server, to end: enter -1");
+                        userInput = Console.ReadLine();
+
+                        string closeSocketString = userInput == "-1" ? "<CloseSocket>" : String.Empty;
+                        byte[] messageSent = Encoding.ASCII.GetBytes($"{userInput} <EOF> {closeSocketString}");
+                        int byteSent = sender.Send(messageSent);
+
+                        //recive message
+                        byte[] messageReceived = new byte[1024];
+                        int byteRecv = sender.Receive(messageReceived);
+                        Console.WriteLine("Message from Server -> {0}",
+                              Encoding.ASCII.GetString(messageReceived,
+                                                         0, byteRecv));
+                    }
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+
+                // Manage of Socket's Exceptions
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
